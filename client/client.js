c9451bcc3879e214f858d5e5098e58c7cf6e1b0c
diegojsk <diego.jun@thunderatz.org>
2020-11-26 19:11:05 -0300

    Add docstring

--- client/client.js
+++ client/client.js
@@ -2,16 +2,16 @@
 PMR3412 - Redes Industriais (2020)
 Diego Jun Sato Kurashima - 10274231
 
-Exercício 2
+Segunda Entrega - Exercício 2
 Client (client.js)
 */
 
 var chat_window = document.getElementById("chat");
 var message_input = document.getElementById("message_input");
-var ws = new WebSocket("ws://localhost:8080");
+var websocket = new WebSocket("ws://localhost:8080");
 
 // Add new messages to message view
-ws.onmessage = function(message){
+websocket.onmessage = function(message){
     chat_window.innerHTML += ('<p>' + message.data + '</p>')
     chat_window.scrollTop = chat_window.scrollHeight;
 }
@@ -20,9 +20,7 @@ ws.onmessage = function(message){
 message_input.onkeyup = function(event){
 
     if (event.keyCode == 13) {
-
-    ws.send(message_input.value);
+    websocket.send(message_input.value);
     message_input.value = "";
-
      }
  }
--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@
 PMR3412 - Redes Industriais (2020)
 Diego Jun Sato Kurashima - 10274231
 
-Exercício 2
+Segunda Entrega - Exercício 2
 Server
 '''
 
@@ -22,6 +22,9 @@ client_dic = {}
 # Enviar mensagem com alvo selecionável
 async def client_message_handler(text, sender):
     '''
+    Processa as mensagens de determinado client e faz o envio
+    :param text: mesagem a ser enviada, obtida pelo método .recv()
+    :param sender: identificar o sender de origem sender.send()
     '''
 
     # Verificar se tem um destino especificado
@@ -30,7 +33,7 @@ async def client_message_handler(text, sender):
         target_end = text.find(">>", 2)
         if target_end  != -1:
             target = text[2:text.find(">>", 2)]
-            message = text[text.find(">>", 2):]
+            text = text[text.find(">>", 2)+2:]
 
         else:
             await sender.send("Para mensagem privada use a notação '>>[nome do usuário]>>' ")
@@ -65,6 +68,9 @@ async def client_message_handler(text, sender):
 # Mensagens do servidor, com escolha de excessões
 async def server_message_handler(text, exception_websocket = []):
     '''
+    Processa mensagens do servidor
+    :param text: mensagem a ser enviada
+    :param exception_websocket: lista com os nomes dos clients a não receberem a mensagem
     '''
     message = text
 
@@ -75,6 +81,9 @@ async def server_message_handler(text, exception_websocket = []):
 
 async def register(websocket, name):
     '''
+    Registra um novo client no dicionário de clientes
+    :param websocket: websocket
+    :param name: nome do client
     '''
     client_dic[websocket] = name
     message = str(name) +  " se juntou à sala :)"
@@ -82,6 +91,8 @@ async def register(websocket, name):
 
 async def unregister(websocket):
     '''
+    Retira da lista um websocket desconectado
+    :param websocket: websocket
     '''
     message = str(client_dic[websocket]) + " saiu da sala :("
     client_dic.pop(websocket)
@@ -91,6 +102,10 @@ async def unregister(websocket):
 #=== CLIENT HANDLER ===
 
 async def client_handler(websocket, path):
+    '''
+    Processa as interações entre client e servidor
+    :param websocket: websocket do client
+    '''
 
     # Novo Client Conectado !!!
     await websocket.send("Bem-vindo à Sala de Bate Papo com WebSockets !!!")
